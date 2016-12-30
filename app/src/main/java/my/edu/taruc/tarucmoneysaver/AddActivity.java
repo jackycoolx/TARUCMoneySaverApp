beb78c82f1d3d165c06587baafa1e7f4835d6785
jackycoolx <ngwk-wa11@student.tarc.edu.my>
2016-12-30 15:40:16 +0800

    7788 Complete

--- app/src/main/java/my/edu/taruc/tarucmoneysaver/AddActivity.java
+++ app/src/main/java/my/edu/taruc/tarucmoneysaver/AddActivity.java
@@ -2,33 +2,103 @@ package my.edu.taruc.tarucmoneysaver;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddActivity extends AppCompatActivity implements View.OnClickListener{
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView captureImageView;
 
+    Button captureButton;
+    Button submitButton;
+    Button cancelButton;
+
+    EditText editFoodname;
+    EditText editRestaurantname;
+    EditText editFoodprice;
+    EditText editFoodcomment;
+
+    RequestQueue requestQueue;
+    String insertURL = "http://tarucmoneysaverdb.esy.es/insertFood.php";
+
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        Button captureButton = (Button)findViewById(R.id.captureImageButton);
+        captureButton = (Button)findViewById(R.id.captureImageButton);
         captureImageView = (ImageView)findViewById(R.id.captureImageView);
 
-        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(this);
+
+        editFoodname = (EditText) findViewById(R.id.foodNameEditText);
+        editRestaurantname = (EditText) findViewById(R.id.restaurantNameEditText);
+        editFoodprice = (EditText) findViewById(R.id.foodPriceEditText);
+        editFoodcomment = (EditText) findViewById(R.id.commentEditText);
 
         //Disable the captureImageButton if the user's phone no camera
         if(!hasCamera())
             captureButton.setEnabled(false);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                StringRequest request = new StringRequest(Request.Method.POST,insertURL,new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        //System.out.println(response.toString());
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                    }
+                }) {
+                    @Override
+                    protected Map<String,String> getParams() throws AuthFailureError {
+                        Map<String, String> parameters = new HashMap<>();
+                        parameters.put("name",editFoodname.getText().toString());
+                        parameters.put("restaurant",editRestaurantname.getText().toString());
+                        parameters.put("price",editFoodprice.getText().toString());
+                        parameters.put("comment",editFoodcomment.getText().toString());
+
+                        return parameters;
+                    }
+                };
+                requestQueue.add(request);
+                Toast.makeText(getApplicationContext(),"Submit Successfully",Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 
     //Check if the user has a camera
@@ -58,7 +128,8 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private void cancelToMain()
+    //Cancel button back to main page function
+    private void backMain()
     {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -70,7 +141,7 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
         switch (v.getId())
         {
             case R.id.cancelButton:
-                cancelToMain();
+                backMain();
                 break;
         }
     }
--- app/src/main/java/my/edu/taruc/tarucmoneysaver/listActivity.java
+++ app/src/main/java/my/edu/taruc/tarucmoneysaver/listActivity.java
@@ -1,13 +1,101 @@
 package my.edu.taruc.tarucmoneysaver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class listActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class listActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView result;
+    RequestQueue requestQueue;
+    String showURL = "http://tarucmoneysaverdb.esy.es/showFoodList.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        //Back button on list page
+        Button backButtonList = (Button) findViewById(R.id.backButtonList);
+        backButtonList.setOnClickListener(this);
+
+        Button searchButtonList = (Button) findViewById(R.id.searchButtonList);
+
+        result = (TextView) findViewById(R.id.showFoodView);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        //Show Food List
+        searchButtonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                System.out.println("ww");
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                        showURL,new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response){
+                        System.out.println(response.toString());
+                        try {
+                            JSONArray foodlists = response.getJSONArray("TMSfoodlist");
+                            for (int x = 0; x < foodlists.length(); x++){
+                                JSONObject foodlist = foodlists.getJSONObject(x);
+
+                                String foodname = foodlist.getString("name");
+                                String restname = foodlist.getString("restaurant");
+                                String foodprice = foodlist.getString("price");
+                                String foodcomment = foodlist.getString("comment");
+
+                                result.append("Food : " + foodname + "\n" +
+                                        "Restaurant : " + restname + "\n" +
+                                        "Food Price : RM" + foodprice+ "\n" +
+                                        "Comment : " + foodcomment + "\n\n");
+                            }
+                            result.append("====================================\n");
+                            result.setMovementMethod(new ScrollingMovementMethod());
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        System.out.append(error.getMessage());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+    }
+
+    private void backMain()
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.backButtonList:
+                backMain();
+                break;
+        }
     }
 }
